e0616d41c94626d871a190ae3fb63017c65cefcf
thebeyondr <jeopatto@gmail.com>
2022-06-20 18:59:01 -0500

    feat: :heavy_plus_sign: Switch to TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <link rel="icon" href="./favicon.ico" />
+                <meta name="msapplication-TileColor" content="#405AE0" />
+                <meta name="theme-color" content="#405AE0" />
+                <meta name="viewport" content="viewport-fit=cover" />
+                <meta
+                    name="description"
+                    content="The onboarding page for the ZKOPRU extension"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
